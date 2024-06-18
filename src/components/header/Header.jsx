ae3812b027673ed0c779cb886ba5e6e9336864b3
Serkan Duran <75946630+1serkanduran@users.noreply.github.com>
2024-06-19 02:31:43 +0300

    başlık değişikliği

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="flex justify-center items-center py-2 px-4 bg-gray-800 mt-2 overflow-hidden">
       <Link to="/" className="flex items-center">
         <img src="/film-icon.png" alt="Film Icon" className="w-12 h-auto mr-4 hover:opacity-80 transition-opacity duration-300" />
-        <h1 className="text-white text-xl md:text-2xl font-bold">Moduna Göre Film Seç!</h1>
+        <h1 className="text-white text-xl md:text-2xl font-bold">Moduna Göre Film</h1>
       </Link>
     </div>
   );
